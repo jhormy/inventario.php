dcfa0cb8c8dcd19c8d9714a379fd95809e4d2de7
jhormy <jhormy.castro@gmail.com>
2025-04-25 10:12:51 -0500

    Primer commit del sistema de inventario

---
+++ controladores/ventas.controlador.php
@@ -0,0 +1,195 @@
+<?php
+
+class ControladorVentas {
+
+	/*=============================================
+	MOSTRAR VENTAS
+	=============================================*/	
+
+	static public function ctrMostrarVentas($item, $valor){
+
+		$tabla = "ventas";
+
+		$respuesta = ModeloVentas::mdlMostrarVentas($tabla, $item, $valor);
+
+		return $respuesta;
+
+	}
+
+	/*=============================================
+	CREAR VENTA
+	=============================================*/	
+
+	public function ctrCrearVenta(){
+
+		if(isset($_POST["nuevaVenta"])){
+
+			/* GUARDAR LA COMPRA */
+			$tabla = "ventas";
+
+			$datos = array("id_vendedor"=>$_POST["idVendedor"],
+						   "id_cliente"=>$_POST["seleccionarCliente"],
+						   "codigo"=>$_POST["nuevaVenta"],
+						   "productos"=>$_POST["listaProductos"],
+						   "impuesto"=>$_POST["nuevoPrecioImpuesto"],
+						   "neto"=>$_POST["nuevoPrecioNeto"],
+						   "total"=>$_POST["totalVenta"],
+						   "metodo_pago"=>$_POST["listaMetodoPago"]);
+
+			$respuesta = ModeloVentas::mdlIngresarVenta($tabla, $datos);
+
+			if($respuesta == "ok"){
+
+				echo'<script>
+
+				localStorage.removeItem("rango");
+
+				swal({
+					  type: "success",
+					  title: "La venta ha sido guardada correctamente",
+					  showConfirmButton: true,
+					  confirmButtonText: "Cerrar"
+					  }).then(function(result){
+								if (result.value) {
+
+								window.location = "ventas";
+
+								}
+							})
+
+				</script>';
+
+			}
+
+		}
+
+	}
+
+	/*=============================================
+	EDITAR VENTA
+	=============================================*/	
+
+	public function ctrEditarVenta(){
+
+		if(isset($_POST["editarVenta"])){
+
+			/* GUARDAR CAMBIOS */
+			$tabla = "ventas";
+
+			$datos = array("id_vendedor"=>$_POST["idVendedor"],
+						   "id_cliente"=>$_POST["seleccionarCliente"],
+						   "codigo"=>$_POST["editarVenta"],
+						   "productos"=>$_POST["listaProductos"],
+						   "impuesto"=>$_POST["nuevoPrecioImpuesto"],
+						   "neto"=>$_POST["nuevoPrecioNeto"],
+						   "total"=>$_POST["totalVenta"],
+						   "metodo_pago"=>$_POST["listaMetodoPago"]);
+
+			$respuesta = ModeloVentas::mdlEditarVenta($tabla, $datos);
+
+			if($respuesta == "ok"){
+
+				echo'<script>
+
+				localStorage.removeItem("rango");
+
+				swal({
+					  type: "success",
+					  title: "La venta ha sido editada correctamente",
+					  showConfirmButton: true,
+					  confirmButtonText: "Cerrar"
+					  }).then(function(result){
+								if (result.value) {
+
+								window.location = "ventas";
+
+								}
+							})
+
+				</script>';
+
+			}
+
+		}
+
+	}
+
+	/*=============================================
+	ELIMINAR VENTA
+	=============================================*/	
+
+	public function ctrEliminarVenta(){
+
+		if(isset($_GET["idVenta"])){
+
+			$tabla = "ventas";
+			$datos = $_GET["idVenta"];
+
+			$respuesta = ModeloVentas::mdlEliminarVenta($tabla, $datos);
+
+			if($respuesta == "ok"){
+
+				echo'<script>
+
+				swal({
+					  type: "success",
+					  title: "La venta ha sido eliminada correctamente",
+					  showConfirmButton: true,
+					  confirmButtonText: "Cerrar"
+					  }).then(function(result){
+								if (result.value) {
+
+								window.location = "ventas";
+
+								}
+							})
+
+				</script>';
+			}
+		}
+	}
+
+	/*=============================================
+	RANGO DE FECHAS
+	=============================================*/	
+
+	static public function ctrRangoFechasVentas($fechaInicial, $fechaFinal){
+
+		$tabla = "ventas";
+
+		$respuesta = ModeloVentas::mdlRangoFechasVentas($tabla, $fechaInicial, $fechaFinal);
+
+		return $respuesta;
+		
+	}
+
+	/*=============================================
+	DESCARGAR XML
+	=============================================*/
+
+	static public function ctrDescargarXML(){
+
+		if(isset($_GET["xml"])){
+
+			$xml = $_GET["xml"];
+
+			return $xml;
+
+		}
+	}
+
+	/*=============================================
+	SUMA TOTAL VENTAS
+	=============================================*/
+
+	public static function ctrSumaTotalVentas(){
+
+		$tabla = "ventas";
+
+		$respuesta = ModeloVentas::mdlSumaTotalVentas($tabla);
+
+		return $respuesta;
+
+	}
+
+}
